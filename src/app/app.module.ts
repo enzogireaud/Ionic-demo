57c628d3ce9d62dc6d92e46c56c8b902c5760f26
furasshujipushii <enzogireauds@gmail.com>
2023-09-14 12:35:50 +0200

    adding the ngif and ngfor to loop over the pokemons array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent,NavbarComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent,NavbarComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
